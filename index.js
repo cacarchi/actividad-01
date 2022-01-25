77d86462ccfbdb8d35e01f9c09d7174aca18cd7a
César Carchi <cacarchi@outlook.com>
2022-01-24 22:06:18 -0500

    Se agrega atributo lorem en la rama hotfix-1

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 class claseA {
-    constructor (foo, bar) {
+    constructor (foo, bar, lorem) {
         this.foo = foo;
         this.bar = bar;
+        this.lorem = lorem;
     }
 }
 class claseB {
